edebd27b47499b2706dd663409f1cc1af4435601
seydur <seydur.rahman82@gmail.com>
2026-02-18 07:43:35 +0600

    database changed

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -91,7 +91,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {"default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))}
+DATABASES = {
+    "default": dj_database_url.config(
+        default="sqlite:///" + str(BASE_DIR / "db.sqlite3"),
+        conn_max_age=600,
+    )
+}
 
 
 # Password validation
